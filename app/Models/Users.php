ab0a839fa353584931e9e9b1952eb83378091f45
HaiLP <lephuhai0511@gmail.com>
2017-12-16 14:41:02 +0700

    Add config mongodb

---
+++ app/Models/Users.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Jenssegers\Mongodb\Eloquent\Model;
+
+/**
+ * @property int $_id
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ */
+class Users extends Model
+{
+
+    protected $primaryKey = 'id';
+
+    protected $connection = 'meta';
+    protected $collection = 'users';
+}
--- config/app.php
+++ config/app.php
@@ -178,6 +178,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         Barryvdh\Debugbar\ServiceProvider::class,
+        Jenssegers\Mongodb\MongodbServiceProvider::class,
 
     ],
 
